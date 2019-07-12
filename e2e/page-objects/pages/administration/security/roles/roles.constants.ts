01676e03ddd89bd83fff1309fa9cdf5d21bb801e
Ali Said <ali.said@aurea.com>
2019-07-12 11:48:57 +0200

    [REM-89896] - New Case

--- e2e/page-objects/pages/administration/security/roles/roles.constants.ts
+++ e2e/page-objects/pages/administration/security/roles/roles.constants.ts
@@ -1,3 +1,10 @@
 export class RolesConstants {
     static readonly pageTitle = 'Manage Roles';
+
+    static readonly elementNames = Object.freeze({
+        role: 'Role',
+        active: 'Active',
+        Delete: 'delete',
+        description: 'Description',
+    });
 }
--- e2e/page-objects/pages/administration/security/roles/roles.helper.ts
+++ e2e/page-objects/pages/administration/security/roles/roles.helper.ts
@@ -1,5 +1,9 @@
 import { StepLogger } from '../../../../../../core/logger/step-logger';
+import { PageHelper } from '../../../../../components/html/page-helper';
+import { ReportWriterReportsPage } from '../../../report-writer-reports-page/report-writer-reports-page.po';
+import { ReferralSourcesPage } from '../../configuration/referral-sources/referral-sources.po';
 
+import { RolesConstants } from './roles.constants';
 import { RolesPage } from './roles.po';
 
 export class RolesHelper {
@@ -7,5 +11,17 @@ export class RolesHelper {
     static async verifyRolesPageDisplayed() {
         StepLogger.subVerification('Verify "Roles" page displayed');
         await RolesPage.rolesPageTitle.verifyDisplayedStatus();
+    }
+
+    static async verifyColumnReporterPage() {
+        await PageHelper.switchToDefaultContentAndIFrame(ReferralSourcesPage.resourceOneIFrame);
+        StepLogger.subVerification(' description  should be displayed');
+        await ReportWriterReportsPage.getCoulumn(RolesConstants.elementNames.description).verifyDisplayedStatus();
+        StepLogger.subVerification(' role  should be displayed');
+        await ReportWriterReportsPage.getCoulumn(RolesConstants.elementNames.role).verifyDisplayedStatus();
+        StepLogger.subVerification(' active  should be displayed');
+        await ReportWriterReportsPage.getCoulumn(RolesConstants.elementNames.active).verifyDisplayedStatus();
+        StepLogger.subVerification(' Delete should be displayed');
+        await ReportWriterReportsPage.getCoulumn(RolesConstants.elementNames.Delete).verifyDisplayedStatus();
     }
 }
